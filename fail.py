3002c779d72a3e5447ae2d8f8ddac176b622ebda
lh1808 <luka.hauck@web.de>
2026-03-22 22:06:43 +0100

    Update fail.py

--- fail.py
+++ fail.py
@@ -1,9 +1,93 @@
-(generic) ubuntu@192.168.0.138 ~/rubin $ pixi install
-Error:   × failed to map conda packages to their PyPI equivalents. This mapping is required when using PyPI dependencies alongside
-  │ conda packages.
-  ├─▶ failed to fetch conda-pypi mapping from remote source
-  ├─▶ error sending request for url (https://conda-mapping.prefix.dev/hash-
-  │   v0/82088a6e4daa33329a30bc26dc19a98c7c1d3f05c0f73ce9845d4eab4924e9e1)
-  ├─▶ client error (Connect)
-  ├─▶ dns error
-  ╰─▶ failed to lookup address information: Name does not resolve
+tuned_baselearner_params.json
+2.74KB
+Path: file:///domino/edv/pvc-hf1kundehuk/mlflow/308961295234797365/f1c78d08b914463689c60ab819a49e26/artifacts/tuned_baselearner_params.json
+{
+  "NonParamDML": {
+    "model_y": {
+      "n_estimators": 301,
+      "learning_rate": 0.10508421338691762,
+      "num_leaves": 13,
+      "max_depth": 5,
+      "min_child_samples": 121,
+      "min_child_weight": 0.0015339162591163618,
+      "subsample": 0.8037724259507192,
+      "subsample_freq": 2,
+      "colsample_bytree": 0.5325257964926398,
+      "min_split_gain": 0.9488855372533332,
+      "reg_alpha": 4.905556676028774,
+      "reg_lambda": 0.18861495878553936
+    },
+    "model_t": {
+      "n_estimators": 301,
+      "learning_rate": 0.10508421338691762,
+      "num_leaves": 13,
+      "max_depth": 5,
+      "min_child_samples": 121,
+      "min_child_weight": 0.0015339162591163618,
+      "subsample": 0.8037724259507192,
+      "subsample_freq": 2,
+      "colsample_bytree": 0.5325257964926398,
+      "min_split_gain": 0.9488855372533332,
+      "reg_alpha": 4.905556676028774,
+      "reg_lambda": 0.18861495878553936
+    },
+    "default": {
+      "n_estimators": 301,
+      "learning_rate": 0.10508421338691762,
+      "num_leaves": 13,
+      "max_depth": 5,
+      "min_child_samples": 121,
+      "min_child_weight": 0.0015339162591163618,
+      "subsample": 0.8037724259507192,
+      "subsample_freq": 2,
+      "colsample_bytree": 0.5325257964926398,
+      "min_split_gain": 0.9488855372533332,
+      "reg_alpha": 4.905556676028774,
+      "reg_lambda": 0.18861495878553936
+    }
+  },
+  "DRLearner": {
+    "model_propensity": {
+      "n_estimators": 301,
+      "learning_rate": 0.10508421338691762,
+      "num_leaves": 13,
+      "max_depth": 5,
+      "min_child_samples": 121,
+      "min_child_weight": 0.0015339162591163618,
+      "subsample": 0.8037724259507192,
+      "subsample_freq": 2,
+      "colsample_bytree": 0.5325257964926398,
+      "min_split_gain": 0.9488855372533332,
+      "reg_alpha": 4.905556676028774,
+      "reg_lambda": 0.18861495878553936
+    },
+    "model_regression": {
+      "n_estimators": 256,
+      "learning_rate": 0.17254716573280354,
+      "num_leaves": 31,
+      "max_depth": 5,
+      "min_child_samples": 35,
+      "min_child_weight": 0.004207053950287938,
+      "subsample": 0.5290418060840998,
+      "subsample_freq": 7,
+      "colsample_bytree": 0.8005575058716043,
+      "min_split_gain": 0.7080725777960455,
+      "reg_alpha": 1.5320059381854043e-8,
+      "reg_lambda": 5.360294728728285
+    },
+    "default": {
+      "n_estimators": 301,
+      "learning_rate": 0.10508421338691762,
+      "num_leaves": 13,
+      "max_depth": 5,
+      "min_child_samples": 121,
+      "min_child_weight": 0.0015339162591163618,
+      "subsample": 0.8037724259507192,
+      "subsample_freq": 2,
+      "colsample_bytree": 0.5325257964926398,
+      "min_split_gain": 0.9488855372533332,
+      "reg_alpha": 4.905556676028774,
+      "reg_lambda": 0.18861495878553936
+    }
+  }
+}
